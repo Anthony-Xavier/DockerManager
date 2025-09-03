666265cf14bee6ad041635cc41adb66a35420ae7
Anthony xavier <anthonyltx12@gmail.com>
2025-09-03 10:48:06 -0300

    fist commit

---
+++ src/test/java/com/xavier/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
